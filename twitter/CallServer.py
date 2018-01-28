58b2cdbbb545568ec21659742464c058369ecb52
JakeWaldner <jakew.waldner@gmail.com>
2018-01-28 12:51:26 -0500

    Poll for Responses

--- twitter/CallServer.py
+++ twitter/CallServer.py
@@ -2,6 +2,15 @@ import urllib.request
 import urllib.parse
 
 def serverCall(tweetText):
-	urllib.request.urlopen("http://server.calstephens.tech:8081/uploadQuery?tweet-text=" + urllib.parse.quote(tweetText)).read()
+	response = urllib.request.urlopen("http://server.calstephens.tech:8081/uploadQuery?tweet-text=" + urllib.parse.quote(tweetText)).read()
 	#True for false
 	#some print
+
+	return response
+
+def responseCall(taskID):
+	assistantResponses = urllib.request.urlopen("http://server.calstephens.tech:8081/deliverAssistantResponses?task-id=" + urllib.parse.quote(taskID)).read()
+	#True for false
+	#some print
+
+	return response

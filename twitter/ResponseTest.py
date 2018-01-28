925cc46ac4d780f3c669f321d998a10010108c1f
JakeWaldner <jakew.waldner@gmail.com>
2018-01-28 02:45:00 -0500

    Only replies back to new tweets one time

--- twitter/ResponseTest.py
+++ twitter/ResponseTest.py
@@ -4,6 +4,10 @@ from twitter_keys import keys
 
 currentTime = datetime.datetime.now()
 
+idFile = open("last_id_replied.py","r")
+last_id_replied = idFile.read()
+idFile.close()
+
 #tweepy setting and handling keys
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
@@ -14,14 +18,24 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-tweets = api.search(q="@Alexa_vs_Siri")
+#old way to search for mentions
+#tweets = api.search(q="@Alexa_vs_Siri")
+
+#consists of all tweets since last id that has been replied, can only do this for up to 20 tweets at a time
+print("lastID: " + last_id_replied)
+mentionTweets = api.mentions_timeline(since_id = last_id_replied)
 
 #for all tweets @'ed to the bot...
-for tweet in tweets:
+#if tweet.lang == "en" restricts responses to english only tweets
+for tweet in mentionTweets:
   tweeterName = tweet.user.screen_name
   #@'ing the tweeterName
-  message = "@" + str(tweeterName) + " " + str(currentTime.hour) + ":"
-  + str(currentTime.minute) + ":" + str(currentTime.second)
-  + " " + "Hey nort, I'm a bot"
+  message = "@" + str(tweeterName) + " " + str(currentTime.hour) + ":" + str(currentTime.minute) + ":" + str(currentTime.second) + " " + "Hey nort, I'm a bot"
+  
+
   #this sends our tweet
   tweet = api.update_status(message, tweet.id)
+  print (str(tweet.id))
+  idFile = open("last_id_replied.py","w")
+  idFile.write(str(tweet.id))
+  idFile.close()

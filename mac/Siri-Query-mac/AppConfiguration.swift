0ca3b74192ddc3724c16a2c7e71f885f994bb98f
Cal Stephens <cal@calstephens.tech>
2018-01-28 10:43:58 -0600

    Add Watson Speech-to-Text API

---
+++ mac/Siri-Query-mac/AppConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  AppConfiguration.swift
+//  Siri-Query-mac
+//
+//  Created by Cal Stephens on 1/28/18.
+//  Copyright © 2018 SiriQuery. All rights reserved.
+//
+
+import Foundation
+
+struct AppConfiguration {
+    
+    static let username = "cal"
+    
+}

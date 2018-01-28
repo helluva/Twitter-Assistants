ae0ffe7cb00e6b24287c83fbf2a8b48d910c9683
JakeWaldner <jakew.waldner@gmail.com>
2018-01-28 01:43:01 -0500

    Created a simple bot that responds to @'ed tweets

---
+++ twitter/twitter_keys.py
@@ -0,0 +1,6 @@
+keys = dict(
+	consumer_key =          '7CchEpt4QJm8LidOMA75kFQfs',
+	consumer_secret =       'ZaEym1Gq5iYtno7AbGj107xRTXYMqXWcRKXvFq26XjcBTGcpw7',
+	access_token =          '957463213322489857-qjl7A17F4m8qIVUm9A5oKgKRiS3wF3Q',
+	access_token_secret =   'HAfnpOmzrLlCqGEPC7XJU5TCYeidiyNebZT7aNMBHu2we',
+)
